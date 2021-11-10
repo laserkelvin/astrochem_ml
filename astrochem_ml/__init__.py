666f466fa40a16cd5e8f0ae27b374f40d69eb061
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-11-10 09:42:43 -0500

    feat: exposing modules in import

--- astrochem_ml/__init__.py
+++ astrochem_ml/__init__.py
@@ -1,5 +1,7 @@
 """Top-level package for Astrochem ML."""
 
+from astrochem_ml import smiles_utils, classes
+
 __author__ = """Kin Long Kelvin Lee"""
 __email__ = 'kin.long.kelvin.lee@gmail.com'
 __version__ = '0.1.0'
