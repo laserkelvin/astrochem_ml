b89bf8da7e6296f55cb6b93395a7a989555626e0
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-11-17 12:50:55 -0500

    dep: removing reference to mol2vec

--- astrochem_ml/smiles/misc.py
+++ astrochem_ml/smiles/misc.py
@@ -1,9 +1,7 @@
-
 from warnings import warn
 
 import numpy as np
 from rdkit import Chem
-from mol2vec import features
 
 
 def canonicize_smiles(smi: str) -> str:
@@ -29,33 +27,13 @@ def canonicize_smiles(smi: str) -> str:
         warn(f"{smi} could not be converted to a `Molecule` object.")
 
 
-def smi_to_vector(smi: str, model, radius: int = 1) -> np.ndarray:
+def generate_cartesian_coords(molecule: Chem.Mol):
     """
-    Given an embedding model and SMILES string, generate the corresponding
-    molecule vector.
+    TODO
 
     Parameters
     ----------
-    smi : str
-        Input SMILES string
-    model : [type]
-        mol2vec object
-    radius : int, optional
-        Radius used for Morgan FPs, by default 1
-
-    Returns
-    -------
-    np.ndarray
-        N-dimensional vector corresponding
-        to the molecule embedding
+    molecule : Chem.Mol
+        [description]
     """
-    # Molecule from SMILES will break on "bad" SMILES; this tries
-    # to get around sanitization (which takes a while) if it can
-    mol = Chem.MolFromSmiles(smi, sanitize=False)
-    mol.UpdatePropertyCache(strict=False)
-    Chem.GetSymmSSSR(mol)
-    # generate a sentence from rdkit molecule
-    sentence = features.mol2alt_sentence(mol, radius)
-    # generate vector embedding from sentence and model
-    vector = features.sentences2vec([sentence], model)
-    return vector
+    pass
